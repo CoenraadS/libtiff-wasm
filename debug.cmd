f6f50f29c7b212871aa451a208212fce7a3ead5b
CoenraadS <CoenraadS@users.noreply.github.com>
2022-12-14 20:58:54 +0100

    Try to create github action (#4)

--- debug.cmd
+++
@@ -1,6 +0,0 @@
-@REM This will launch docker in interactive mode
-set dir=%~dp0
-
-docker build --pull --rm -f "Dockerfile" -t libtiffwasm:latest "." 
-docker rm -f libtiffwasm-dev
-docker run -it --entrypoint bash --name libtiffwasm-dev -v %dir%build:/build -v %dir%out:/out libtiffwasm:latest
